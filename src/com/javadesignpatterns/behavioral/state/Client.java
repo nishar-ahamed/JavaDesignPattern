af0b700aa63d89ef6d0487b02b533abda5b3b5ae
nishar-ahamed <nishar.kwd@gmail.com>
2019-07-06 22:59:55 +0530

    add state design pattern

---
+++ src/com/javadesignpatterns/behavioral/state/Client.java
@@ -0,0 +1,13 @@
+package com.javadesignpatterns.behavioral.state;
+
+public class Client {
+
+    public static void main(String[] args) {
+        Order order = new Order();
+        
+        order.paymentSuccessful();
+        order.dispatched();
+        order.delivered();
+        order.cancel();
+    }
+}
---
+++ src/com/javadesignpatterns/behavioral/state/OrderState.java
@@ -0,0 +1,7 @@
+package com.javadesignpatterns.behavioral.state;
+
+//Abstract state
+public interface OrderState {
+
+	double handleCancellation();
+}
